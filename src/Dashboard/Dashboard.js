14058b5830e726c62ff56d79fa25e47c1e823cbf
zain <zainjhere@gmail.com>
2023-09-02 19:43:12 +0500

    Added auth handshake for socket connection

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -25,7 +25,7 @@ const Dashboard = ({setUserDetails}) => {
 
     else {
       setUserDetails(JSON.parse(userDetails));
-      connectWithSocketServer();
+      connectWithSocketServer(JSON.parse(userDetails));
     }
 
   }, [])
